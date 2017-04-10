023a1b4b98eebe18e63c55c2f7804a540faf1ca8
Taylor Feinstein <Taylor Feinstein>
2017-04-10 18:21:53 -0500

    Trivia Game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -65,7 +65,7 @@ console.log(questionSetter);
 
 // Create On Click Events
 var userGuess = document.getElementsByTagName("BUTTON");
-userGuess;
+userGuess.onclick = function() { alert('clicked')};
 console.log(userGuess);
 
 // Time Container
